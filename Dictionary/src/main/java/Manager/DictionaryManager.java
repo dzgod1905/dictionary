73a0dcf3b7f20b9b89138e9141e3a67e88b9b288
dzgod1905 <99169476+dzgod1905@users.noreply.github.com>
2023-10-23 14:40:31 +0700

    feat: data txt added

---
+++ Dictionary/src/main/java/Manager/DictionaryManager.java
@@ -0,0 +1,21 @@
+package Manager;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import Models.*;
+
+public class DictionaryManager {
+  private static Dictionary dictionary = new Dictionary();
+
+  private void insertFromFile(){
+    
+  }
+
+  public DictionaryManager(){
+    insertFromFile();
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
