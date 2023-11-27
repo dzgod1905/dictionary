fd8774bb1ba5af21c074abe5b27f821f2c9cc227
Tân Nguyên <tannguyen8b@gmail.com>
2023-11-27 11:35:05 +0700

    feat: add GUI warnings.

--- Dictionary/src/main/java/Views/DictionaryApplication/DictionaryController.java
+++ Dictionary/src/main/java/Views/DictionaryApplication/DictionaryController.java
@@ -5,7 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
@@ -13,10 +15,14 @@ import javafx.util.Duration;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class DictionaryController implements Initializable {
     private String current = "";
+    public static boolean CHANGED = false;
+    public static boolean EDITING = false;
+    private final Warnings warnings = new Warnings();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -33,7 +39,17 @@ public class DictionaryController implements Initializable {
     }
 
     private void quit() {
-        DictionaryManager.getInstance().exportToFile();
+        if (EDITING) {
+            warnings.showWarningInfo("Warning", "Bạn có thay đổi chưa được ghi nhận.");
+            return;
+        }
+        if (CHANGED) {
+            Alert exportConfirm = warnings.alertConfirmation("Export dictionary",
+                    "Bạn đã thay đổi nội dung từ điển.\nBạn có muốn lưu lại thay đổi trên tệp văn bản không?");
+            Optional<ButtonType> option = exportConfirm.showAndWait();
+            if (option.isEmpty()) return;
+            if (option.get() == ButtonType.OK) DictionaryManager.getInstance().exportToFile();
+        }
         System.exit(0);
     }
 
@@ -45,6 +61,10 @@ public class DictionaryController implements Initializable {
     @FXML
     private void showComponent(String path) {
         if (current.equals(path)) return;
+        if (EDITING) {
+            warnings.showWarningInfo("Cảnh báo", "Bạn có thay đổi chưa được ghi nhận.");
+            return;
+        }
         current = path;
         try {
             AnchorPane component = FXMLLoader.load(Objects.requireNonNull(
